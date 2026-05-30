94cacaa3499555232fa2f8a6105c17738f2c9338
Dhruv Dabur <your_email@example.com>
2026-05-30 12:47:14 +0530

    Limit dev hardware branch to camera and hardware

--- src/av_control_guide/run_test_suites.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Run test suites!!
-
-rem === pytest based test runner ===
-rem -l (--showlocals); show local variables when test failed
-rem --durations=0: show ranking of test durations
-pytest -l --durations=0
